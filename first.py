87e34cb0a1e4a61ecbbe7f58383ccee3f28977fd
MEERSANAKHAN <mrkhan20outlook2005@gmail.com>
2024-11-07 11:29:43 -0800

    Add files via upload

---
+++ first.py
@@ -0,0 +1,22 @@
+num1=15
+num2=6
+num3=10
+#calculate sum
+sum=num1+num2+num3
+print("sum:",sum)
+#calculate diffrence
+diffrence=num1-num2-num3
+print("difference:,difference")
+#calculate product
+product=num1*num2*num3
+print("product",product)
+#calculate average
+average=(num1+num2+num3)/3
+print("average",average)
+#area of triangle
+width=6
+height=7
+area=0.5*width*height
+print("triangle area",area)
+
+
